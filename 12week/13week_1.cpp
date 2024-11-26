60194ca52b7de3785b180be880cd93b9947ccac5
ggcon <39198298a@gmail.com>
2024-11-26 11:16:39 +0900

    문제정리

--- 12week/13week_1.cpp
+++ 12week/13week_1.cpp
@@ -4,23 +4,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <conio.h>
 #include <windows.h>
 
-void Initialize() {
+//초기화 함수
+void Init() {
 	system("cls");
-	printf("초기화...\n");
+	printf("초기화"\n);
 }
 
+//렌더링 함수
 void Render() {
-	printf("렌더링...\n");
+	printf("렌더링\n");
 }
 
+//업데이트 함수(키 입력을 받음)
 void Update() {
 	if (_kbhit()) {
 		char key = _getch();
-		printf("키 입력 : %c\n", key);
+		printf("키 입력 : %c/n", key);
 
 		if (key == 'q') {
 			printf("종료\n");
@@ -29,22 +31,18 @@ void Update() {
 	}
 }
 
+//게임루프 함수
 void GameLoop() {
-	Initialize();
-
-	clock_t start_time = clock(); // 현재 시간을 기록
+	Init();
 	while (1) {
-		Update(); // Update는 계속 호출하여 키 입력을 처리
-		// 현재 시간과 시작 시간의 차이를 확인하여 1초마다 렌더링
-		if ((clock() - start_time) >= CLOCKS_PER_SEC) { // CLOCKS_PER_SEC는 1초
-			Render();
-			start_time = clock(); // 시간을 초기화하여 다음 1초를 기다림
-		}
+		Update();
+		Render();
+		//Sleep(1000 / 60); 이거 없으면 cpu 불탐
 	}
 }
 
-
-int main() {
+//메인 함수
+int main () {
 	GameLoop();
 	return 0;
 }
--- 12week/13week_2.cpp
+++ 12week/13week_2.cpp
@@ -4,56 +4,59 @@
 //60분의 1초로 델타타임 만들기
 //cpu가 불타지 않게 남은 시간은 sleep()으로 쉬게
 
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <conio.h>
 #include <windows.h>
+#include <time.h>
 
-void Initialize() {
+//초기화 함수
+void Init() {
 	system("cls");
-	printf("초기화...\n");
+	printf("초기화"\n);
 }
 
+//렌더링 함수
 void Render() {
-	printf("렌더링...\n");
+	printf("렌더링\n");
 }
 
-void Update() {
+//업데이트 함수(키 입력을 받음)
+void Update(float deltaTime) {
 	if (_kbhit()) {
 		char key = _getch();
-		printf("키 입력 : %c\n", key);
+		printf("키 입력 : %c (delta_time : %.2fms)\n", key, deltaTime);
 
-		if (key == '27') {
+		if (key == 'q') {
 			printf("종료\n");
 			exit(0);
 		}
 	}
 }
 
-void Loop() {
-	Initialize();
+//게임루프 함수
+void GameLoop() {
+	Init();
 
-	// 프레임 타이머
 	clock_t lastTime = clock();
-	int fps = 60;
-	double frameTime = 1000.0 / fps;
+	const int targetFrameTime = 1000 / 60;
 
-	clock_t start_time = clock();
 	while (1) {
-		Update();
+		clock_t currentTime = clock();
+		float deltaTime = (float)(currentTime - lastTime) * 1000 / CLOCKS_PER_SEC;
 
-		if ((clock() - start_time) >= CLOCKS_PER_SEC) {
-			Render();
-			start_time = clock();
-			Sleep(16);
+		Update(deltaTime);
+		Render();
+		
+		int sleepTime = targetFrameTime - deltaTime;
+		if (sleepTime > 0) {
+			Sleep(sleepTime);
 		}
-
 	}
 }
 
+//메인 함수
 int main() {
-	Loop();
+	GameLoop();
 	return 0;
 }
